2d214b0c703f032138497150b3f4142d25804d5c
kaloyan.ivanov <kaloyan.ivanov88@gmail.com>
2022-06-30 10:37:06 +0300

    added comments

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -11,5 +11,4 @@ public class Constants {
     public static final String HEADER_MESSAGE = "Cube   Front   Back    Left    Right   Top    Bottom";
     public static final String PROMPT_MESSAGE = "Please enter your cubes:";
     public static final String PERMUTATION_COUNT = "Valid permutations found: ";
-
 }
